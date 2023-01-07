0fcb8f5693d4514a8529fc071baac68900208af2
kspilaszek <krystian.spilaszek@morizon-gratka.pl>
2023-01-07 20:04:06 +0100

    kurs python-a

---
+++ pythonTraining/hello.py
@@ -0,0 +1,21 @@
+
+# Zadanie 1 zmienne
+s = 38
+t = 3
+v = s / t
+print(v)
+
+K = 1000
+p = 4
+n = 5
+
+#zadanie zmienne 2
+z = K * (1 + p / 100 * 12) ** n
+print(z)
+
+# zadanie 3
+a = 9
+b = 12
+c = (a ** 2 + b ** 2) ** 0.5
+total = 2 * c
+print("-" * int(c))
